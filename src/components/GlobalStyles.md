0b0b980a6ac39d192da01d6689ae3909ff0aa8ed
Christian Marin <chrismarin0607@gmail.com>
2025-06-08 20:40:52 -0400

    first commit

---
+++ src/components/GlobalStyles.md
@@ -0,0 +1,20 @@
+## Variables de colores
+
+```js
+const primaryColor = "rgb(3, 4, 5)";
+const primaryForeground = "rgb(240, 247, 244)";
+const secondaryForeground = "rgb(180, 126, 162)";
+const extraSmall = "12px";
+const small = "14px";
+const base = "16px";
+const medium = "20px";
+const large = "24px";
+const xlarge = "32px";
+const xxlarge = "48px";
+const desktop = "1920px";
+const laptop = "1440px";
+const smallLaptop = "1024px";
+const tablet = "768px";
+const mobile = "480px";
+const smallMobile = "375px";
+```
---
+++ src/lib/logos/index.js
@@ -0,0 +1,21 @@
+import SupabaseLogo from "../../assets/svg/supabase.svg";
+import TailwindLogo from "../../assets/svg/tailwindcss.svg";
+import SvelteLogo from "../../assets/svg/svelte.svg";
+import CSS3Logo from "../../assets/svg/css3.svg";
+import HTML5Logo from "../../assets/svg/html5.svg";
+import ReactLogo from "../../assets/svg/react.svg";
+import NextjsLogo from "../../assets/svg/nextjs.svg";
+import GitLogo from "../../assets/svg/git.svg";
+import GitHubLogo from "../../assets/svg/github.svg";
+
+export default {
+  SupabaseLogo,
+  TailwindLogo,
+  SvelteLogo,
+  CSS3Logo,
+  HTML5Logo,
+  ReactLogo,
+  NextjsLogo,
+  GitLogo,
+  GitHubLogo,
+};
